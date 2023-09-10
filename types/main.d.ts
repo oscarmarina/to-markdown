103750818097922e6015abeb9b565bc0df693619
oscar marina <oscar7600@outlook.com>
2023-09-10 17:28:26 +0200

    chore: initial commit

---
+++ types/main.d.ts
@@ -0,0 +1,67 @@
+import { Declaration, Module } from 'custom-elements-manifest/schema';
+import { Node, Parent } from 'mdast';
+
+enum Declarations {
+  mixins = 'mixins',
+  variables = 'variables',
+  functions = 'functions',
+  exports = 'exports'
+}
+
+enum Sections {
+  mainDescription = 'main-description',
+  mainHeading = 'main-heading',
+  superClass = 'super-class',
+  fields = 'fields',
+  methods = 'methods',
+  staticFields = 'static-fields',
+  staticMethods = 'static-methods',
+  slots = 'slots',
+  events = 'events',
+  attributes = 'attributes',
+  cssProperties = 'css-properties',
+  cssParts = 'css-parts',
+  mixins = 'mixins'
+}
+
+type OptionalDeclarations = `${Declarations}`;
+type OptionalSections = `${Sections}`;
+
+export interface Options {
+  private?: 'details'|'hidden'|'all';
+  headingOffset?: number;
+  mainDescription?: boolean;
+  omitSections: OptionalSections[];
+  omitDeclarations: OptionalDeclarations[];
+  classNameFilter: string | (() => string);
+}
+
+export interface Descriptor {
+  heading: string;
+  get: (x: T[keyof T]) => string;
+  cellType?: (value: string) => Node;
+  exportKinds?: {
+    'js'?: string;
+    'custom-element-definition'?: string;
+  }
+};
+
+export type CurriedTableFn =
+  (options: Options) =>
+    <T extends Declaration>(
+      title: string,
+      names: (keyof T)|Descriptor[],
+      decls: T[],
+      options?: {
+        headingLevel: number;
+        filter: (...args: any[]) => boolean;
+      },
+    ) => Node[];
+
+export type MakeModuleDocFn =
+  (mod: Module, options: Options) => Parent;
+
+/**
+ * Renders a custom elements manifest as Markdown
+ */
+export declare function customElementsManifestToMarkdown(manifest: Package, options: Options): string;
